0e476eec8396a028de4e8e57cb9bd6e430a44b81
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-02-24 11:02:27 +0000

    Update version and copyright in the JS file

--- RenderWhitespaceOnGithub.user.js
+++ RenderWhitespaceOnGithub.user.js
@@ -1,7 +1,7 @@
 /**
 The MIT License (MIT)
 
-Copyright (c) 2017 Gleb Mazovetskiy
+Copyright (c) 2017-2018 Gleb Mazovetskiy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -25,7 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // @name            Render Whitespace on GitHub
 // @description     Renders spaces as · and tabs as → in all the code on GitHub.
 // @namespace       https://github.com/glebm
-// @version         1.3.9
+// @version         1.3.10
 // @author          Gleb Mazovetskiy <glex.spb@gmail.com>
 // @license         MIT
 // @domain          github.com
